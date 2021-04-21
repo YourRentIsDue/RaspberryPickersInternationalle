2b811f459170a52f111798996c305b3ef01e36a9
YourRentIsDue <simas.lyn@gmail.com>
2021-04-21 18:42:53 +0300

    Added requirements.txt for dependancies and fixed the call on main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ loc = r'Sensors/Test-Data-1.xlsx'
 wb = load_workbook(loc)
 sheet = wb['Sheet2']
 
-sensor1 = LightSensor("Light_" + str(sheet['B3'].value), str(sheet['B2'].value), sheet)
+sensor1 = Sensors.LightSensor.LightSensor("Light_" + str(sheet['B3'].value), str(sheet['B2'].value), sheet)
 
 print(sensor1.getReading())
 
